de6e50f3caffec89d724327afdd39540e27b78e5
Arthur Gibert <arthurgibert@free.fr>
2024-01-24 22:30:58 +0100

    Ajout de la méthode takeOrder dans la classe Commande

--- src/Commande.java
+++ src/Commande.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -6,13 +7,23 @@ import fr.fms.data.*;
 import fr.fms.entities.*;
 
 public class Commande {
+	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		String[] categories = {"Entrée", "Plat", "Accompagnement", "Boisson", "Dessert"};
+		ArrayList<ArrayList<Article>> listMenu = new ArrayList<>();
+		listMenu.add(Menu.getEntry());
+		listMenu.add(Menu.getDish());
+		listMenu.add(Menu.getSide());
+		listMenu.add(Menu.getDrink());
+		listMenu.add(Menu.getDessert());
+		
 		System.out.println("Bonjour et bienvenue ! Combien êtes-vous ?");
-		String errorMsg = "Veuillez saisir un nombre valable: ";
-		validationService(scanner, 4, errorMsg);
+		int nbrMenu = validationService(scanner, Integer.MAX_VALUE, "Veuillez saisir un nombre valable: ");
+		takeOrder(scanner, nbrMenu, listMenu, categories);
 	}
 	
+	// Méthode qui vérifie les entrées saisie par l'utilisateur:
 	public static int validationService(Scanner scanner, int maxValue, String errorMsg) {
 		int userChoice = 0;
 		
@@ -30,5 +41,26 @@ public class Commande {
 			}
 		}
 		return userChoice;
+	}
+	
+	// Méthode qui permet de prendre une commande
+	public static void takeOrder(Scanner scanner, int nbrMenu, ArrayList<ArrayList<Article>> listMenu, String[] categories) {
+		Order order = new Order();
+		
+		for(int i = 0; i < nbrMenu; i++) {	
+			for(int j = 0; j < listMenu.size(); j++) {
+				System.out.println("------  " + categories[j] + "  ------");
+				Menu.displayMenu(listMenu.get(j));
+				System.out.print("Veuillez saisir votre choix : ");
+				int userChoice = validationService(scanner, listMenu.get(j).size(), "Veuillez saisir un choix valable: ");
+				order.addArticle(listMenu.get(j).get(userChoice - 1));;
+				System.out.println();
+			}
+			
+			System.out.println(order);
+			System.out.println("Voici votre commande :");
+			System.out.println(order.getArticleList());
+			System.out.println(order.getTotalPrice());
+		}
 	}
 }

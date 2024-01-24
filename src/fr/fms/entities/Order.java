e635f48fb182052c98c8b4716b38eba36e3fad3a
Arthur Gibert <arthurgibert@free.fr>
2024-01-24 13:35:10 +0100

    Ajout de la classe Order

---
+++ src/fr/fms/entities/Order.java
@@ -0,0 +1,64 @@
+package fr.fms.entities;
+
+import java.util.ArrayList;
+
+public class Order {
+	// Propriétés de classe:
+	private ArrayList<Article> articleList;
+	private double totalPrice;
+	private static int nextId = 1;
+	private int id = 1;
+	
+	// Constructeurs:
+	public Order(ArrayList<Article> articleList, double totalPrice) {
+		this.articleList = articleList;
+		this.totalPrice = calculateTotalPrice(articleList);
+		this.id = nextId;
+		nextId++;
+	}
+	
+	// Méthode:
+	private double calculateTotalPrice(ArrayList<Article> articleList) {
+		double total = 0;
+		for(Article article : articleList) {
+			total = total + article.getPrice();
+		}
+		return total;
+	}
+	
+	public String toString() {
+		if(this.articleList.size() == 0) {
+			return "La commande numéro " + getId() + " est vide !";
+		} else {
+			return "Commande numéro " + getId() + " - Coût total : " + getTotalPrice();
+		}
+	}
+	// Accesseurs:
+	public ArrayList<Article> getArticleList() {
+		return articleList;
+	}
+
+	public void setArticleList(ArrayList<Article> articleList) {
+		this.articleList = articleList;
+	}
+
+	public double getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(double totalPrice) {
+		if(totalPrice < 0) {
+			throw new IllegalArgumentException("Le coût total de la commande ne peut être inférieur à 0€");
+		} else {
+			this.totalPrice = totalPrice;
+		}
+	}
+	
+	public int getId() {
+		return id;
+	}
+	// On retire le setter de l'id pour pas que celui-ci soit modifiable.
+	//	public void setId(int id) {
+	//		this.id = id;
+	//	}
+}
